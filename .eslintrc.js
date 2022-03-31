84155a8b916732030f735953a7c9337e34c98ddd
Thezero <yonghu520@outlook.com>
2022-03-31 20:31:27 +0800

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
   parser: 'vue-eslint-parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
@@ -9,12 +14,31 @@ module.exports = {
       tsx: true
     }
   },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      rules: {
+        'no-undef': 'off'
+      }
+    }
+  ],
   rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'comma-dangle': ['off', 'always-multiline'], // 添加结尾的逗号
+    'no-var': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-void': 'error',
+    camelcase: 'off',
+    //"comma-dangle": ['off', "always-multiline"], // 添加结尾的逗号
     'no-param-reassign': ['error', { props: false }], // 参数不可改变，属性可改变
+
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-types': 0,
     '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -22,9 +46,50 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': [
       'error',
       { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
-    ],
+    ], // 变量未使用
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     semi: ['error', 'always'],
     indent: ['error', 2, { SwitchCase: 1, flatTernaryExpressions: true }], // 使用两个空格，switch强制缩进default:0
-    camelcase: 'off'
+
+    // vue
+    'vue/html-indent': 'off',
+    'vue/html-self-closing': ['off'],
+    'vue/no-v-html': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/custom-event-name-casing': 'error',
+    'vue/no-deprecated-props-default-this': 'off',
+    'vue/require-explicit-emits': 'off',
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 4,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/max-len': [
+      'error',
+      {
+        code: 150,
+        template: 100,
+        tabWidth: 2,
+        comments: 150,
+        ignorePattern: '',
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+        ignoreHTMLAttributeValues: true,
+        ignoreHTMLTextContents: true
+      }
+    ],
+
+    // prettier
+    'prettier/prettier': 'error'
   }
 };
