76384e8a810829850d4da45a6ef985d82390d9b7
alqmc <Yonghu520@outlook.com>
2022-06-24 14:36:08 +0800

    fix: zip

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -3,12 +3,13 @@ import { run, withTask } from '@alqmc/build-utils';
 import { copyFiles } from './copyfile';
 import { buildStyles } from './buildStyle';
 import { buildTs, buildVue } from './build-chrome';
+import { zip } from './buildZip';
 export default series(
   // withTask('update:version', () => run('pnpm run update:version')),
   withTask('clear', () => run('pnpm run clear')),
   buildVue,
   buildTs,
   buildStyles,
-  copyFiles
-  // createZip
+  copyFiles,
+  zip
 );
