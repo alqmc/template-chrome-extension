3a0451dffd9a41d60e9525d281327e8c1323e07d
alqmc <Yonghu520@outlook.com>
2022-06-24 14:43:45 +0800

    fix: devDependencies

--- build/buildStyle.ts
+++ build/buildStyle.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { dest, src } from 'gulp';
 import cleanCss from 'gulp-clean-css';
-import chalk from 'chalk';
+import { log } from '@alqmc/build-utils';
 import gulpLess from 'gulp-less';
 import autoprefixer from 'gulp-autoprefixer';
 import { buildOutpath, enterPath } from './utils/path';
@@ -12,9 +12,9 @@ export const buildStyles = () => {
     .pipe(
       cleanCss({}, (d) => {
         console.log(
-          `${chalk.cyan(d.name)}: ${chalk.yellow(
-            d.stats.originalSize / 1000
-          )} KB -> ${chalk.green(d.stats.minifiedSize / 1000)} KB`
+          `${log.info(d.name)}: ${log.warning(
+            `${d.stats.originalSize / 1000}`
+          )} KB -> ${log.success(`${d.stats.minifiedSize / 1000}`)} KB`
         );
       })
     )
